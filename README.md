662f850cc0a5ed55513b7227061991179884ee60
Yong <work.qu@outlook.com>
2019-10-24 21:22:00 +0800

    add xlua project and update test result

--- README.md
+++ README.md
@@ -4,22 +4,29 @@
 以github上的release版本为准：
 slua-1.7.0
 tolua-1.0.8.591
+xlua-2.1.14
 
-其中slua使用的是原生lua5.1，tolua使用的是luajit-2.1
+其中slua使用的是原生lua5.1，tolua使用的是luajit-2.1，xlua分为两个版本lua5.3和luajit2.1
 unity版本2018.4.2f1，编译设置为il2cpp
-测试例子来自slua中的perf示例程序，启动程序后依次执行一次
+测试例子修改自slua中的perf示例程序，启动程序后依次执行一次
 
 测试结果（华为荣耀9）：
-case    test1   test2   test3   test4   test5   test6   test7
-tolua   0.754   0.820   5.157   4.302   1.260   1.254   1.185
-slua    1.369   2.907   10.527  6.035   1.705   4.567   4.058
+case    test1   test2   test3   test4   test5   test6
+tolua   0.754   0.820   5.257   4.302   1.160   1.254
+slua    1.369   2.907   10.327  5.935   1.658   4.367
+xlua53  0.998   1.423   9.743   4.379   1.117   1.740
+xluajit 0.471   0.742   3,891   4.014   1.046   0.794
 
 测试结果（iPhone 6s）：
-case    test1   test2   test3   test4   test5   test6   test7
-tolua   0.173   0.232   1.113   1.897   0.568   0.366   0.346
-slua    0.447   1.140   3.680   2.950   0.794   1.729   1.605
+case    test1   test2   test3   test4   test5   test6
+tolua   0.173   0.232   1.113   1.897   0.568   0.366
+slua    0.447   1.140   3.680   2.950   0.794   1.729
+xlua53
+xluajit
 
-测试结果（网易MuMu，PC-i7-3.0GHZ-CPU-16G-RAM-Win10-x64）：
-case    test1   test2   test3   test4   test5   test6   test7
-tolua   0.112   0.134   0.003   1.179   0.358   0.0005  0.0006
-slua    0.362   0.759   2.353   1.971   0.861   1.189   1.022
+测试结果（UnityEditor，PC-i7-3.0GHZ-CPU-16G-RAM-Win10-x64）：
+case    test1   test2   test3   test4   test5   test6
+tolua   0.116   0.123   0.534   1.639   0.399   0.244
+slua    0.766   0.942   2.059   3.655   1.079   0.831
+xlua53  0.126   0.177   1.211   1.608   0.391   0.346
+xluajit 0.103   0.146   1.006   1.514   0.396   0.190
---
+++ xlua-2.1.14-luajit/Assets/benchmark/benchmark.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using XLua;
+public class benchmark : MonoBehaviour
+{
+    LuaEnv _luaEnv;
+    bool inited = false;
+    // Use this for initialization
+    void Start()
+    {
+        Application.logMessageReceived += this.log;
+
+        var startMem = System.GC.GetTotalMemory(true);
+
+        var start = Time.realtimeSinceStartup;
+        _luaEnv = new LuaEnv();
+        var script = Resources.Load<TextAsset>("benchmark").bytes;
+        Debug.Log("start cost: " + (Time.realtimeSinceStartup - start));
+
+        var endMem = System.GC.GetTotalMemory(true);
+        Debug.Log("startMem: " + startMem + ", endMem: " + endMem + ", " + "cost mem: " + (endMem - startMem));
+        _luaEnv.DoString(script);
+        inited = true;
+    }
+
+    string logText = "";
+    void log(string cond, string trace, LogType lt)
+    {
+        logText += cond;
+        logText += "\n";
+    }
+
+    void OnGUI()
+    {
+        if (!inited)
+            return;
+
+        if (GUI.Button(new Rect(10, 10, 120, 50), "Test1"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test1");
+            UnityEngine.Profiling.Profiler.BeginSample("test1");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        if (GUI.Button(new Rect(10, 100, 120, 50), "Test2"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test2");
+            UnityEngine.Profiling.Profiler.BeginSample("test2");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        if (GUI.Button(new Rect(10, 200, 120, 50), "Test3"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test3");
+            UnityEngine.Profiling.Profiler.BeginSample("test3");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        if (GUI.Button(new Rect(10, 300, 120, 50), "Test4"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test4");
+            UnityEngine.Profiling.Profiler.BeginSample("test4");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        if (GUI.Button(new Rect(200, 10, 120, 50), "Test5"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test5");
+            UnityEngine.Profiling.Profiler.BeginSample("test5");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        if (GUI.Button(new Rect(200, 100, 120, 50), "Test6"))
+        {
+            logText = "";
+            var func = _luaEnv.Global.Get<LuaFunction>("test6");
+            UnityEngine.Profiling.Profiler.BeginSample("test6");
+            func.Call();
+            UnityEngine.Profiling.Profiler.EndSample();
+        }
+
+        GUI.Label(new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height), logText);
+    }
+}
